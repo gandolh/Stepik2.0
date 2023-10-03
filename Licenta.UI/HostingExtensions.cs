1463fa188271cd97a79a9e374710192eddff1180
Gandolh <29356778+gandolh@users.noreply.github.com>
2023-10-03 06:00:49 +0300

    added localization

---
+++ Licenta.UI/HostingExtensions.cs
@@ -0,0 +1,30 @@
+﻿using Licenta.SDK.Localization;
+using Microsoft.Extensions.Localization;
+
+namespace Licenta.UI
+{
+    public static class HostingExtensions
+    {
+
+        #region localization
+        /// <summary>
+        /// Adaugare traduceri in aplicatie
+        /// </summary>
+        /// <param name="services"></param>
+        private static void AddLocalization(this WebApplicationBuilder builder)
+        {
+            // Register localization as usual
+            builder.Services.AddLocalization(options =>
+            {
+                options.ResourcesPath = Path.Join("Localization", "Resources");
+            });
+            // And add out custom factory
+            builder.Services.AddSingleton
+                <IStringLocalizerFactory, JsonStringLocalizerFactory>();
+        }
+        #endregion
+
+
+
+    }
+}
