a45f16f16767d158623d2b9161cdbf3d8d9d11fa
gandolh <29356778+gandolh@users.noreply.github.com>
2023-10-06 12:58:49 +0300

    commit intermediar UI

---
+++ Licenta/Licenta.UI/Comp/Index/CourseList.razor.cs
@@ -0,0 +1,9 @@
+﻿using Components.UI;
+using Licenta.Sdk.Localization;
+
+namespace Licenta.UI.Comp.Index
+{
+    public partial class CourseList : BaseLicentaComponent<ComponentsResource>
+    {
+    }
+}
