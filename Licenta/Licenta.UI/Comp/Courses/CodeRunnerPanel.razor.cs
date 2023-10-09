75aa0f8d11846228d8a04672d39a779fa25d152f
Gandolh <29356778+gandolh@users.noreply.github.com>
2023-10-10 00:34:43 +0300

    integrated monaco editor on page.

--- Licenta/Licenta.UI/Comp/Courses/CodeRunnerPanel.razor.cs
+++ Licenta/Licenta.UI/Comp/Courses/CodeRunnerPanel.razor.cs
@@ -1,6 +1,23 @@
-﻿namespace Licenta.UI.Comp.Courses
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Licenta.UI.Comp.Courses
 {
     public partial class CodeRunnerPanel
     {
+        [Inject] private IJSRuntime JsRuntime { get; set; } = default!;
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (firstRender)
+            {
+                JsRuntime.InvokeVoidAsync("MonacoEditorUtils.initialize", "codePanelId",
+                    "function x() {\n" + 
+	                "   console.log(\"Hello world!\");\n" +
+                    "}", "javascript");
+
+            }
+            base.OnAfterRender(firstRender);
+        }
     }
 }
