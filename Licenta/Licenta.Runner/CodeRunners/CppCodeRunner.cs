86fedc7f65c4d9defa4bbeb913a21f88b04afea1
Gandolh <29356778+gandolh@users.noreply.github.com>
2023-10-27 02:24:17 +0300

    cpp runner

--- Licenta/Licenta.Runner/CodeRunners/CppCodeRunner.cs
+++ Licenta/Licenta.Runner/CodeRunners/CppCodeRunner.cs
@@ -1,12 +1,87 @@
 ﻿using Licenta.Sdk.Models.Data;
+using System.Diagnostics;
 
 namespace Licenta.Runner.CodeRunners
 {
     public class CppCodeRunner : ICodeRunner
     {
-        public Task<CodeRunResult> Run(CodeRunReq req)
+        public async Task<CodeRunResult> Run(CodeRunReq req)
         {
-            throw new NotImplementedException();
+            string filename = Guid.NewGuid().ToString();
+            string cppPath = "/code_to_run/" + filename + "_cpp.cpp";
+            string outPath = "/code_to_run/" + filename + "_cpp.out";
+
+
+            Directory.CreateDirectory("/code_to_run");
+
+            await Compile(cppPath, outPath, req.Code );
+            return await RunCompilled(outPath, req.Input);
+        }
+        private async Task<CodeRunResult> RunCompilled(string outPath, string InputData)
+        {
+            ProcessStartInfo processStartInfo = new()
+            {
+                FileName = outPath,
+                //Arguments = outPath,
+                RedirectStandardOutput = true,
+                RedirectStandardInput = true,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            Process process = new()
+            {
+                StartInfo = processStartInfo
+            };
+            process.Start();
+            if (string.IsNullOrEmpty(InputData) == false)
+            {
+                string[] inputs = InputData.Split("\n");
+                foreach (string input in inputs)
+                    process.StandardInput.WriteLine(input);
+            }
+            string result = await process.StandardOutput.ReadToEndAsync();
+            string error = await process.StandardError.ReadToEndAsync();
+            process.StandardInput.Close();
+            await process.WaitForExitAsync();
+            File.Delete(outPath);
+
+            Console.WriteLine("error: " + error);
+            Console.WriteLine("result: " + result);
+
+            return new CodeRunResult()
+            {
+                Result = result,
+                Error = error
+            };
+        }
+        private async Task Compile(string cppPath, string outPath, string code)
+        {
+            string command = $"g++";
+            string processArgument = $"{cppPath} -o {outPath}";
+
+            await File.WriteAllTextAsync(cppPath, code);
+            ProcessStartInfo processStartInfo = new()
+            {
+                FileName = command,
+                Arguments = processArgument,
+                RedirectStandardError = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            Process process = new()
+            {
+                StartInfo = processStartInfo
+            };
+            process.Start();
+            string compilleError = await process.StandardError.ReadToEndAsync();
+            await process.WaitForExitAsync();
+            File.Delete(cppPath);
+
+
+            Console.WriteLine("error: " + compilleError);
+            if (string.IsNullOrEmpty(compilleError) == false)
+                throw new ArgumentException("Couldn't compille: %s", compilleError);
         }
     }
 }
--- Licenta/Licenta.Runner/Program.cs
+++ Licenta/Licenta.Runner/Program.cs
@@ -8,41 +8,61 @@ using Licenta.Sdk.Models.Data;
 //var host = builder.Build();
 //host.Run();
 
-ICodeRunner pythonRunner = new PythonCodeRunner();
+//ICodeRunner pythonRunner = new PythonCodeRunner();
 
-var x = await pythonRunner.Run(new CodeRunReq()
-{
-    Code = """
-    # Define a function to sum two numbers
-    def add_numbers(a, b):
-        return a + b
-
-    # Declare two integer variables
-    num1 = int(input("Enter the first number: "))
-    num2 = int(input("Enter the second number: "))
-
-    # Call the function and store the result in a variable
-    result = add_numbers(num1, num2)
-
-    # Print the result
-    print(f"{num1} + {num2} = {result}")
-    
-    """,
-    Input = "2\n3",
-    Language = CodeLanguage.Python
-});
-
-
-//ICodeRunner cppCodeRunner = new CppCodeRunner();
-//cppCodeRunner.Run(new CodeRunReq()
+//var x = await pythonRunner.Run(new CodeRunReq()
 //{
 //    Code = """
-//            #include <iostream>
-//            int main() {
-//                std::cout << "Hello World!";
-//                return 0;
-//            }
-//            """,
-//    Input = "",
-//     Language = CodeLanguage.Cpp
+//    # Define a function to sum two numbers
+//    def add_numbers(a, b):
+//        return a + b
+
+//    # Declare two integer variables
+//    num1 = int(input("Enter the first number: "))
+//    num2 = int(input("Enter the second number: "))
+
+//    # Call the function and store the result in a variable
+//    result = add_numbers(num1, num2)
+
+//    # Print the result
+//    print(f"{num1} + {num2} = {result}")
+
+//    """,
+//    Input = "2\n3",
+//    Language = CodeLanguage.Python
 //});
+
+
+ICodeRunner cppCodeRunner = new CppCodeRunner();
+await cppCodeRunner.Run(new CodeRunReq()
+{
+    Code = """
+            #include <iostream>
+
+            // Function to sum two numbers
+            int addNumbers(int a, int b) {
+                return a + b;
+            }
+
+            int main() {
+                int num1, num2;
+
+                // Read two integer variables from the user
+                std::cout << "Enter the first number: ";
+                std::cin >> num1;
+
+                std::cout << "Enter the second number: ";
+                std::cin >> num2;
+
+                // Call the function and store the result in a variable
+                int result = addNumbers(num1, num2);
+
+                // Print the result
+                std::cout << "The sum of " << num1 << " + " << num2 << " = " << result << std::endl;
+
+                return 0;
+            }
+            """,
+    Input = "2\n3",
+    Language = CodeLanguage.Cpp
+});
