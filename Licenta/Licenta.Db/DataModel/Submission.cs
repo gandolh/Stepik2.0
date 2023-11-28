188fadb8e18a3e517829e0c7717ae0287b431db7
Gandolh <29356778+gandolh@users.noreply.github.com>
2023-11-28 17:52:03 +0200

    progressed submision repository

--- Licenta/Licenta.Db/DataModel/Submission.cs
+++ Licenta/Licenta.Db/DataModel/Submission.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Licenta.Db.Data
+﻿namespace Licenta.Db.Data
 {
-    internal class Submission
+    public class Submission
     {
         public int Id { get; set; } = 0;
         // % out of 100
---
+++ Licenta/Licenta.Db/Repositories/SubmissionRepository.cs
@@ -0,0 +1,37 @@
+﻿using Licenta.Db.Data;
+using Licenta.Db.Seeder.Interfaces;
+
+namespace Licenta.Db.Repositories
+{
+    public class SubmissionRepository : BaseRepository<Submission>
+    {
+        public SubmissionRepository(IDapperDbClient dbClient) : base(dbClient)
+        {
+        }
+
+        public override async Task CreateTableAsync()
+        {
+            string sql = $"""
+                DROP TABLE IF EXISTS {_tableName};
+
+                CREATE TABLE {_tableName} (
+                    Id SERIAL PRIMARY KEY,
+                    Points INT,
+                    StudentId INT,
+                    ExerciseId INT
+                );
+                """;
+            await _dbClient.ExecuteAsync(sql);
+        }
+
+        public override async Task InsertAsync(Submission data)
+        {
+            string sql = $"""
+                INSERT INTO {_tableName}
+                (Firstname, Lastname, Password) 
+                VALUES (@Firstname,@Lastname,@Password);
+            """;
+            var rowsAffected = await _dbClient.ExecuteAsync(sql, data);
+        }
+    }
+}
