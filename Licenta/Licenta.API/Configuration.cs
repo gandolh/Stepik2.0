e324e9710947dbf4ecd5e42258de5025adf86212
Gandolh <29356778+gandolh@users.noreply.github.com>
2023-12-01 00:41:59 +0200

    integrated Endpoint-Extension pattern in minimal API

---
+++ Licenta/Licenta.API/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace Licenta.API
+{
+    public static class Configuration
+    {
+        public static void RegisterServices(this WebApplicationBuilder builder)
+        {
+            builder.Services
+                   .AddEndpointsApiExplorer()
+                   .AddSwaggerGen();
+        }
+
+        public static void RegisterMiddlewares(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger()
+                   .UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+        }
+    }
+}
---
+++ Licenta/Licenta.API/Endpoints/Courses.cs
@@ -0,0 +1,11 @@
+﻿namespace Licenta.API.Endpoints
+{
+    public static class Courses
+    {
+        public static void RegisterCoursesEndpoints(this IEndpointRouteBuilder routes)
+        {
+            var users = routes.MapGroup("/api/courses");
+            users.MapGet("", () => new List<object>());
+        }
+    }
+}
