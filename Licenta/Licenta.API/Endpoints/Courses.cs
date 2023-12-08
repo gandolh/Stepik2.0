91ceb7da365c60b540f09479b2e508fb31fb18a1
Gandolh <29356778+gandolh@users.noreply.github.com>
2023-12-08 17:10:53 +0200

    removed NiceAdmin theme from repository

--- Licenta/Licenta.API/Endpoints/Courses.cs
+++ Licenta/Licenta.API/Endpoints/Courses.cs
@@ -1,11 +1,13 @@
-﻿namespace Licenta.API.Endpoints
+﻿using Licenta.SDK.Models;
+
+namespace Licenta.API.Endpoints
 {
     public static class Courses
     {
         public static void RegisterCoursesEndpoints(this IEndpointRouteBuilder routes)
         {
-            var users = routes.MapGroup("/api/courses");
-            users.MapGet("", () => new List<object>());
+            var users = routes.MapGroup(ApiEndpoints.Courses.Prefix);
+            users.MapGet(ApiEndpoints.Courses.GetAll, () => new List<object>());
         }
     }
 }
---
+++ Licenta/Licenta.SDK/Models/ApiEndpoints.cs
@@ -0,0 +1,22 @@
+﻿namespace Licenta.SDK.Models
+{
+    public static class ApiEndpoints
+    {
+        public static BaseEndpointType Courses  = new BaseEndpointType()
+        {
+            Prefix = "/api/course",
+            GetAll=$"/all",
+            GetById= $"/one"
+        };
+    }
+
+    public class BaseEndpointType
+    {
+        public string Prefix = "";
+        public string GetAll = "";
+        public string GetById = "";
+        public string Create = "";
+        public string Update = "";
+        public string Delete = "";
+    }
+}
