42561d1594c434f1b237087c24e27c8e7eff75d0
Gandolh <29356778+gandolh@users.noreply.github.com>
2024-01-16 17:24:33 +0200

    Added navbar

---
+++ Licenta/Licenta.UI/Components/Layout/Navbar.razor.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Licenta.UI.Components.Layout
+{
+    public partial class Navbar
+    {
+        [Inject] public IJSRuntime JSRuntime { get; set; } = default!;
+
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+
+            if(firstRender) {
+                await JSRuntime.InvokeVoidAsync("Main.initializeNav");
+            }
+            await base.OnAfterRenderAsync(firstRender);
+        }
+
+    }
+}
---
+++ Licenta/Licenta.UI/wwwroot/js/main.js
@@ -0,0 +1,9 @@
+const initializeNav = () => {
+        var elems = document.querySelectorAll('.sidenav');
+        var instances = M.Sidenav.init(elems, {});
+}
+
+
+var Main = {
+    initializeNav: initializeNav
+}
