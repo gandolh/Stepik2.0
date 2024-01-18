037ff7131348f31b1eb3fd83ddea89d2ec1f6577
Gandolh <29356778+gandolh@users.noreply.github.com>
2024-01-18 16:44:23 +0200

    Added quiz exercise component

---
+++ Licenta/Licenta.UI/Components/Courses/QuizExercisePanel.razor.cs
@@ -0,0 +1,21 @@
+﻿using Licenta.SDK.Models.Dtos;
+using Microsoft.AspNetCore.Components;
+
+namespace Licenta.UI.Components.Courses
+{
+    public partial class QuizExercisePanel
+    {
+        [Parameter] public required ExerciseDto Exercise { get; set; }
+        private int _selectedIndex = 0;
+        private bool? isCorect = null;
+
+        private void HandleCheckQuiz()
+        {
+            isCorect = (Exercise.QuizVariants[_selectedIndex].IsCorrect == true);
+        }
+
+        private void ChangeSelected(int index) {
+            _selectedIndex = index;
+        }
+    }
+}
