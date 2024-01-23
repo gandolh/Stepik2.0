8a63cceb4c58db44ba03663994e1cc5b2914d260
Gandolh <29356778+gandolh@users.noreply.github.com>
2024-01-23 12:49:59 +0200

    Added layout for view one. Implemented cancel btn

---
+++ Licenta/Licenta.UI/Components/Backoffice/ShowAllLayout.razor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace Licenta.UI.Components.Backoffice
+{
+    public partial class ShowAllLayout
+    {
+        [Parameter] public RenderFragment ChildContent { get; set; } = default!;
+        [Inject] public NavigationManager NavManager { get; set; } = default!;
+
+        private string GetAllUrl()
+        {
+            int index = NavManager.Uri.IndexOf("/one");
+            if(index != -1)
+            return NavManager.Uri.Substring(0, index);
+            return NavManager.Uri;
+        }
+    }
+}
