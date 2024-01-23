b93a7abbca6f2e386c79d619243230e2b1b2ad7b
Gandolh <29356778+gandolh@users.noreply.github.com>
2024-01-23 15:06:51 +0200

    initially sorted array by id

--- Licenta/Licenta.UI/Data/DataTableJson.cs
+++ Licenta/Licenta.UI/Data/DataTableJson.cs
@@ -14,24 +14,27 @@ namespace Licenta.UI.Data
 
         internal void ImportOverride(List<FullCodeEvaluationEntryDto> elts)
         {
-            Headings = ["Enunciation", "Input", "ExpectedResult", ""];
+            elts.Sort((e1, e2) => e1.Id - e2.Id);
+            Headings = ["Id", "Enunciation", "Input", "ExpectedResult", ""];
             int collectionCount = elts.Count();
             int headingsCount = Headings.Count();
             Data = new string[collectionCount][];
             for (int i = 0; i < collectionCount; i++)
             {
+                int index = -1;
                 Data[i] = new string[headingsCount];
-                Data[i][0] = elts[i].Exercise.Enunciation;
-                Data[i][1] = elts[i].Input;
-                Data[i][2] = elts[i].ExpectedResult;
-                Data[i][3] = "crud/codeeval/one/"+ elts[i].Id;
+                Data[i][++index] = elts[i].Id.ToString();
+                Data[i][++index] = elts[i].Exercise.Enunciation;
+                Data[i][++index] = elts[i].Input;
+                Data[i][++index] = elts[i].ExpectedResult;
+                Data[i][++index] = "crud/codeeval/one/"+ elts[i].Id;
             }
         }
 
         internal void ImportOverride(List<CourseDto> elts)
         {
-            
-            Headings = ["Name", "Teachers", "Students", ""];
+            elts.Sort((e1, e2) => e1.Id - e2.Id);
+            Headings = ["Id", "Name", "Teachers", "Students", ""];
             int collectionCount = elts.Count();
             int headingsCount = Headings.Count();
             Data = new string[collectionCount][];
@@ -45,103 +48,123 @@ namespace Licenta.UI.Data
                     ? elts[i].Students[0].FullName + "," + elts[i].Students[1].FullName + "..."
                     : string.Join(",", elts[i].Students.Select(el => el.FullName));
 
+                int index = -1;
                 Data[i] = new string[headingsCount];
-                Data[i][0] = elts[i].Name;
-                Data[i][1] = teacherList;
-                Data[i][2] = studentList;
-                Data[i][3] = "crud/course/one/" + elts[i].Id;
+                Data[i][++index] = elts[i].Id.ToString();
+                Data[i][++index] = elts[i].Name;
+                Data[i][++index] = teacherList;
+                Data[i][++index] = studentList;
+                Data[i][++index] = "crud/course/one/" + elts[i].Id;
 
             }
         }
 
         internal void ImportOverride(List<ExerciseDto> elts)
         {
-            Headings = ["Enunciation", "SampleInput", "IsCodeRunner", ""];
+            elts.Sort((e1, e2) => e1.Id - e2.Id);
+            Headings = ["Id", "Enunciation", "SampleInput", "IsCodeRunner", ""];
             int collectionCount = elts.Count();
             int headingsCount = Headings.Count();
             Data = new string[collectionCount][];
             for (int i = 0; i < collectionCount; i++)
             {
+                int index = -1;
                 Data[i] = new string[headingsCount];
-                Data[i][0] = elts[i].Enunciation;
-                Data[i][1] = elts[i].SampleInput;
-                Data[i][2] = elts[i].IsCodeRunner.ToString();
-                Data[i][3] = "crud/exercise/one/" + elts[i].Id;
+                Data[i][++index] = elts[i].Id.ToString();
+                Data[i][++index] = elts[i].Enunciation;
+                Data[i][++index] = elts[i].SampleInput;
+                Data[i][++index] = elts[i].IsCodeRunner.ToString();
+                Data[i][++index] = "crud/exercise/one/" + elts[i].Id;
             }
         }
 
         internal void ImportOverride(List<LessonDto> elts)
         {
-            Headings = ["Name", "Body", ""];
+            elts.Sort((e1, e2) => e1.Id - e2.Id);
+            Headings = ["Id", "Name", "Body", ""];
             int collectionCount = elts.Count();
             int headingsCount = Headings.Count();
             Data = new string[collectionCount][];
             for (int i = 0; i < collectionCount; i++)
             {
+                int index = -1;
                 Data[i] = new string[headingsCount];
-                Data[i][0] = elts[i].Name;
-                Data[i][1] = elts[i].Body;
-                Data[i][2] = "crud/lesson/one/" + elts[i].Id;
+                Data[i][++index] = elts[i].Id.ToString();
+                Data[i][++index] = elts[i].Name;
+                Data[i][++index] = elts[i].Body;
+                Data[i][++index] = "crud/lesson/one/" + elts[i].Id;
             }
         }
 
         internal void ImportOverride(List<ModuleDto> elts)
         {
-            Headings = ["Name", ""];
+            elts.Sort((e1, e2) => e1.Id - e2.Id);
+            Headings = ["Id", "Name", ""];
             int collectionCount = elts.Count();
             int headingsCount = Headings.Count();
             Data = new string[collectionCount][];
             for (int i = 0; i < collectionCount; i++)
             {
+                int index = -1;
                 Data[i] = new string[headingsCount];
-                Data[i][0] = elts[i].Name;
-                Data[i][1] = "crud/module/one/" + elts[i].Id;
+                Data[i][++index] = elts[i].Id.ToString();
+                Data[i][++index] = elts[i].Name;
+                Data[i][++index] = "crud/module/one/" + elts[i].Id;
             }
         }
 
         internal void ImportOverride(List<FullQuizVariantDto> elts)
         {
-            Headings = ["Enunciation", "Text", "IsCorrect", ""];
+            elts.Sort((e1, e2) => e1.Id - e2.Id);
+            Headings = ["Id", "Enunciation", "Text", "IsCorrect", ""];
             int collectionCount = elts.Count();
             int headingsCount = Headings.Count();
             Data = new string[collectionCount][];
             for (int i = 0; i < collectionCount; i++)
             {
+                int index = -1;
                 Data[i] = new string[headingsCount];
-                Data[i][0] = elts[i].Exercise.Enunciation;
-                Data[i][1] = elts[i].Text;
-                Data[i][2] = elts[i].IsCorrect.ToString();
-                Data[i][3] = "crud/quizvar/one/" + elts[i].Id;
+                Data[i][++index] = elts[i].Id.ToString();
+                Data[i][++index] = elts[i].Exercise.Enunciation;
+                Data[i][++index] = elts[i].Text;
+                Data[i][++index] = elts[i].IsCorrect.ToString();
+                Data[i][++index] = "crud/quizvar/one/" + elts[i].Id;
             }
         }
 
         internal void ImportOverride(List<TeacherDto> elts)
         {
-            Headings = ["Firstname", "Lastname", ""];
+            elts.Sort((e1, e2) => e1.Id - e2.Id);
+            Headings = ["Id", "Firstname", "Lastname", ""];
             int collectionCount = elts.Count();
             int headingsCount = Headings.Count();
             Data = new string[collectionCount][];
             for (int i = 0; i < collectionCount; i++)
             {
+                int index = -1;
                 Data[i] = new string[headingsCount];
-                Data[i][0] = elts[i].Firstname;
-                Data[i][1] = elts[i].Lastname;
-                Data[i][2] = "crud/student/one/" + elts[i].Id;
+                Data[i][++index] = elts[i].Id.ToString();
+                Data[i][++index] = elts[i].Firstname;
+                Data[i][++index] = elts[i].Lastname;
+                Data[i][++index] = "crud/student/one/" + elts[i].Id;
             }
         }
 
         internal void ImportOverride(List<StudentDto> elts)
         {
-            Headings = ["Firstname", "Lastname", ""];
+            elts.Sort((e1, e2) => e1.Id - e2.Id);
+            Headings = ["Id", "Firstname", "Lastname", ""];
             int collectionCount = elts.Count();
             int headingsCount = Headings.Count();
             Data = new string[collectionCount][];
             for (int i = 0; i < collectionCount; i++)
             {
+                int index = -1;
                 Data[i] = new string[headingsCount];
-                Data[i][0] = elts[i].Firstname;
-                Data[i][1] = elts[i].Lastname;
-                Data[i][2] = "crud/teacher/one/" + elts[i].Id;
+                Data[i][++index] = elts[i].Id.ToString();
+                Data[i][++index] = elts[i].Firstname;
+                Data[i][++index] = elts[i].Lastname;
+                Data[i][++index] = "crud/teacher/one/" + elts[i].Id;
             }
         }
     }
